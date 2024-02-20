4fc61677b235ee9e1b1a652b0445a8b9f7ab2468
Douglasjose3 <douglasjose3@gmail.com>
2024-02-19 23:04:20 -0800

    aula sobre objetos

---
+++ aula_objetos/aplique_herancas.js
@@ -0,0 +1,43 @@
+/*/funcionário
+function Pessoa(nome) { //instância/abstração de pessoa
+    this.nome = nome;
+    this.dizOi = function() { //metodo
+        console.log(this.nome + " diz! olá, tudo bem por aí?");
+    }
+}
+
+function Funcionario(nome, cargo, salario) { //instância de funcionário ---- atribuir as informações do Funcionário a Pessoa precisamos tratar a herança.
+    this.nome = nome;
+    this.cargo = cargo;
+    this.salario = salario;
+}
+
+const pessoa1 = new Pessoa("Maria"); //atribui o nome Maria para a instância Pessoa
+const funcionário1 = new Funcionario("Maria", "Front-end", "R$5.000,00");
+pessoa1.dizOi(); //evocando o metodo
+
+console.log(pessoa1);
+console.log(funcionário1); */
+
+
+//cadastro de funcionários
+function Pessoa(name, nacionalidade, idade) { //instância/abstração de pessoas
+    this.name = name;
+    this.nacionalidade = nacionalidade;
+    this.idade = idade;
+}
+
+function Funcionario(name, nacionalidade, idade, cargo, skills, salário, admissão) { //instIancia/abstração de funcionários
+    this.cargo = cargo;
+    this.skills = skills;
+    this.salário = salário;
+    this.admissão = admissão;
+
+    Pessoa.call(this, name, nacionalidade, idade);
+}
+
+const douglasPessoa = new Pessoa("Douglas Rodrigues José", "Brasileiro", "47 anos");
+const douglasFuncionario = new Funcionario("Douglas Rodrigues José", "Brasileiro", "47", "Engenheiro front-end", "Designer, Fotógrafo, Professor de fotografia", 30000, "05-05-2024");
+
+console.log(douglasPessoa);
+console.log(douglasFuncionario);
