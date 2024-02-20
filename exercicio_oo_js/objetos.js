c0d156fea5c57a0f9b1543a3e3be69d9a1aec5b8
Douglasjose3 <douglasjose3@gmail.com>
2024-02-20 12:10:11 -0800

    tentei, tentei e não consegui fazer multiplas henranças

--- exercicio_oo_js/objetos.js
+++ exercicio_oo_js/objetos.js
@@ -5,18 +5,29 @@ function Contatos(nome, cpf, endereco, telefone) {
     this.telefone = telefone;
 }
 
-function Empresa(nome, cpf, endereco, telefone, cnpj, razaoSocial, endereco, capitalSocial, quantosFuncionarios, espacoFisico) {
-    this.cnpj = cnpj;
-    this.razaoSocial = razaoSocial;
-    this.endereco = endereco;
-    this.capitalSocial = capitalSocial;
-    this.quantosFuncionarios = quantosFuncionarios;
-    this.espacoFisico = espacoFisico;
+function Mapeamento(nome, cpf, endereco, telefone, contasAPagar, contasAReceber, quantasNotasEmite, temCartaoDeCredito) {
+    this.contasAPagar = contasAPagar;
+    this.contasAReceber = contasAReceber;
+    this.quantasNotasEmite = quantasNotasEmite;
+    this.temCartaoDeCredito = temCartaoDeCredito;
 
     Contatos.call(this, nome, cpf, endereco, telefone);
 }
 
-const contatoEmpresaMark = new Contatos("Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000");
-const mark = new EmpresaMkt("Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000", "000.000.000/0001-00", "Mark", "Nome da rua, x - Bairro - Cidade - CEP", 100000, 20, "sim");
+function Empresa(cnpj, razaoSocial, capitalSocial, quantosFuncionarios, espacoFisico, contasAPagar, contasAReceber, quantasNotasEmite, temCartaoDeCredito) {
+    this.cnpj = cnpj;
+    this.razaoSocial = razaoSocial;
+    this.capitalSocial = capitalSocial;
+    this.quantosFuncionarios = quantosFuncionarios;
+    this.espacoFisico = espacoFisico;
 
-console.log(mark);
+    Mapeamento.call(this, contasAPagar, contasAReceber, quantasNotasEmite, temCartaoDeCredito);
+}
+
+const contatoEmpresaMark = new Contatos("Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000");
+const mapeamentoMark = new Mapeamento("Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000", 150, 40, 40, "sim");
+const empresaMark = new Empresa("000.000.000/0001-00", "Mark", 100000, 20, "sim", "Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000");
+
+
+
+console.log(empresaMark);
