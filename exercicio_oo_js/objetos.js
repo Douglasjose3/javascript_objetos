6f658b8672fde323d1801eeaf9c32a20aa21b168
Douglasjose3 <douglasjose3@gmail.com>
2024-02-20 13:02:57 -0800

    ainda não deu certo

--- exercicio_oo_js/objetos.js
+++ exercicio_oo_js/objetos.js
@@ -26,7 +26,7 @@ function Empresa(cnpj, razaoSocial, capitalSocial, quantosFuncionarios, espacoFi
 
 const contatoEmpresaMark = new Contatos("Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000");
 const mapeamentoMark = new Mapeamento("Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000", 150, 40, 40, "sim");
-const empresaMark = new Empresa("000.000.000/0001-00", "Mark", 100000, 20, "sim", "Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000");
+const empresaMark = new Empresa("000.000.000/0001-00", "Mark", 100000, 20, "sim", "Fulano", "000.000.000-00", "Nome da rua, x - Bairro - Cidade - CEP", "(11) 90000-0000", 150, 40, 40, "sim");
 
 
 
